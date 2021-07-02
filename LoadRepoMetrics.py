cd1b805ef46e7a7d089a2f9ac9eae294c56136de
goodwillpunning <goodwillpunning>
2021-07-02 16:27:26 +0000

    Load Repo Metrics
    This commit adds a notebook that pulls metrics about repos belonging to the `delta-io` organization, like:
    1. Repo identification (id, name, title, description, owner)
    2. Repo metrics (stargazer count, watcher count, open issues count, forks count, size, last update)
    3. Repo collaborators

---
+++ LoadRepoMetrics.py
@@ -0,0 +1,83 @@
+# Databricks notebook source
+# DBTITLE 1,Libraries
+from ghapi.core import GhApi
+import datetime
+
+# COMMAND ----------
+
+# DBTITLE 1,Global Vars
+GITHUB_ORG_NAME = 'delta-io'
+GITHUB_REPO_NAME = 'delta'
+DATABRICKS_SECRETS_SCOPE = 'demo'
+DATABRICKS_SECRETS_NAME = 'delta-github-metrics-pat'
+TARGET_SCHEMA_NAME = 'delta_github_metrics'
+REPOS_TABLE_NAME = 'repos'
+
+# COMMAND ----------
+
+# Ensure that the target schema is created
+spark.sql("CREATE DATABASE IF NOT EXISTS %s" % TARGET_SCHEMA_NAME)
+
+# COMMAND ----------
+
+# Create a GitHub API client
+github_pat = dbutils.secrets.get(DATABRICKS_SECRETS_SCOPE, DATABRICKS_SECRETS_NAME)
+ghapi = GhApi(token=github_pat)
+
+# COMMAND ----------
+
+# DBTITLE 1,Pull a list of collaborators for each repo
+from pyspark.sql.functions import udf, col, lit
+from pyspark.sql.types import ArrayType, StringType
+
+@udf(ArrayType(StringType()))
+def list_collaborators(owner, repo, token):
+  """Returns a list of collaborators for a given repo."""
+  try:
+    ghapi = GhApi(token)
+    ghapi.repos.list_collaborators(owner, repo)
+    collaborators = ['UserA', 'UserB', 'UserC']
+  except:
+    collaborators = []
+  return collaborators
+
+# COMMAND ----------
+
+# DBTITLE 1,List Repos
+star# Grab delta.io repos information from GitHub API
+repos = ghapi.repos.list_for_org(GITHUB_ORG_NAME)
+
+# Parse repos information into a meaningful table structure
+parsed_repos_list = [{
+  'id': repo['id'],
+  'owner': repo['owner']['login'],
+  'owner_id': repo['owner']['id'],
+  'name': repo['name'],
+  'full_name': repo['full_name'],
+  'description': repo['description'],
+  'stargazers_count': repo['stargazers_count'],
+  'watchers_count': repo['watchers_count'],
+  'open_issues_count': repo['open_issues'],
+  'forks_count': repo['forks_count'],
+  'size': repo['size'],
+  'language': repo['language'],
+  'license': repo['license']['name'],
+  'updated': repo['updated_at']} for repo in repos]
+repos_df = spark.createDataFrame(parsed_repos_list, 'id long, owner string, owner_id long, name string, full_name string, description string, stargazers_count long, watchers_count long, open_issues_count long, forks_count long, size long, language string, license string, updated string')\
+  .withColumn('collaborators', list_collaborators(col('owner'), col('name'), lit(github_pat)))
+
+# Write delta.io repos information to a managed Delta table
+repos_df.write.format('delta').mode('overwrite').saveAsTable('%s.%s' % (TARGET_SCHEMA_NAME, REPOS_TABLE_NAME))
+
+# COMMAND ----------
+
+display(
+  spark.sql(f"""
+  select *
+    from {TARGET_SCHEMA_NAME}.{REPOS_TABLE_NAME}
+  """)
+)
+
+# COMMAND ----------
+
+
